c50ca5fc9968a17b1407c8555c2c8ef7dcb7c139
Mazen Imara <mazen.imara@gmail.com>
2018-03-23 16:33:20 +0100

    add database class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+
 #include <iostream>
+#include <string>
+//#include <windows.h>
+#include <mysql.h>
 
 using namespace std;
 // all method headers:
@@ -12,9 +16,36 @@ using namespace std;
 
 // end mohamed
 
+// mazen database class
+class Database{
+public:
+    MYSQL* conn;
+    MYSQL_ROW row;
+    MYSQL_RES *res;
+    int qstate;
+    Database(){
+        conn = mysql_init(0);
+        if(conn)
+            cout<<"connection object ok, conn="<<conn<<endl;
+        else
+            cout<<"conn object problem: "<<mysql_error(conn);
+
+        conn = mysql_real_connect(conn,"localhost","root","","c_test",0,NULL,0);
+
+        if(conn)
+            cout<<"connect to data base  successfully..."<<endl;
+        else
+            cout<<"conn object problem: "<<mysql_error(conn);
+    }
+    void close(){
+        mysql_close(conn);
+    }
+};
+// end mazen database class
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout<< "hello world " << endl;
     return 0;
 }
