21c699dbb373141c619fba13f00e2c745a5932e1
Gabriel Bunting <Gabriel_89_angel@hotmail.com>
2018-03-27 14:46:02 +0200

    Timestamp created

--- main.cpp
+++ main.cpp
@@ -2,12 +2,33 @@
 #include <iostream>
 #include <string>
 #include <database.h>
+#include <gtime.h>
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
-
 int main()
 {
-   Database db;
+    CustomTime t(10000);
+
+    cout<< t.timeStamp + 1000 << endl;
+    cout<< t.showDate()<< endl;
+    cout<< t.showTime()<< endl;
+
+
     return 0;
 }
+
+int main1(){
+/*
+  time_t base = 1142085600;
+    struct tm* tm = localtime(&base);
+    tm->tm_mday += i;
+    time_t next = mktime(tm);
+    std::cout << ctime(&next);
+
+ */
+return 0;
+}
+
