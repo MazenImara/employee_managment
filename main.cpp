3b86f9924db3800d28a90998988ab6cc9024c8ff
Mohamad Alnabki <alnabki@yahoo.com>
2018-04-06 11:43:28 +0200

    merged from testdmmy to nidal

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
-//#include <login.h>
+#include <login.h>
 #include <iomanip>
 #include <stdlib.h>
 #include <gtime.h>
+#include <employee.h>
+#include <project.h>
+#include <projectMenu.h>
 
 using namespace std;
-/*
+
 void createDatabase(){
 MYSQL* conn;
     MYSQL_ROW row;
@@ -20,7 +23,7 @@ MYSQL* conn;
         cout<<"connection object ok, conn="<<conn<<endl;
     else
         cout<<"conn object problem: "<<mysql_error(conn);
-    conn = mysql_real_connect(conn,"localhost","root","password",NULL,0,NULL,0);
+    conn = mysql_real_connect(conn,"localhost","root","",NULL,0,NULL,0);
 
     if(conn)
         cout<<"test without database  ok, conn="<<conn<<endl;
@@ -83,7 +86,8 @@ MYSQL* conn;
 
         //create task
         {
-        query="CREATE TABLE employee_managment.task(`id` int not null AUTO_INCREMENT, `title` VARCHAR(255), `status` VARCHAR(255), `time_spend` VARCHAR(255), `created` VARCHAR(255), `start` VARCHAR(255), `project_id` int, `employee_id` int, PRIMARY KEY (id))";
+
+        query="CREATE TABLE employee_managment.task(`id` int not null AUTO_INCREMENT, `title` VARCHAR(255), `status` VARCHAR(255), `time_spend` INT, `endtemp` TIMESTAMP, `starttemp` TIMESTAMP , `project_id` int, `employee_id` int, PRIMARY KEY (id));";
         }
          q = query.c_str();
         qstate = mysql_query(conn,q);
@@ -125,15 +129,91 @@ MYSQL* conn;
         mysql_close(conn);
 
 }
-*/
 
-int main1()
+
+void PrintMessage(string message, bool printTop = true, bool printBottom = true)
+{
+	if (printTop)
+	{
+
+		cout << "+----------------------------------------+" << endl;
+		cout << "|";
+	}
+	else
+	{
+		cout << "|";
+	}
+	bool front = true;
+	for (int i = message.length(); i < 40; i++)
+	{
+		if (front)
+		{
+			message = " " + message;
+		}
+		else
+		{
+			message = message + " ";
+		}
+		front = !front;
+	}
+	cout << message.c_str();
+	if (printBottom)
+	{
+		cout << "|" << endl;
+		cout << "+----------------------------------------+" << endl;
+	}
+	else
+	{
+		cout << "|" << endl;
+	}
+}
+
+void AdminMenu();
+void ManageEmployeeMenu();
+void EmployeeMenu();
+void ManageProjectMenu();
+void ManageTaskMenu();
+void manageProjectMenu();
+
+
+void showAllProject()
 {
 
-    CustomTime c;
-    cout << c.date() << "  " << c.Time() << endl;
-    c.getTimestampDate("2018/10/11 23:15:13");
-    //c.getTimestampDate("2017/07/26 00:00:00");
+}
+int main(){
+createDatabase();
+
+}
+int main1()
+{
+    int n;
+    string courseId;
+	do {
+		system("cls");
+		PrintMessage("EMPLOYEE MANAGEMENT");
+		PrintMessage("                      ", false, false);
+		PrintMessage("1. LOGIN              ", false, false);
+		PrintMessage("                      ", false, false);
+		PrintMessage("0. EXIT               ", false, false);
+		PrintMessage("                      ", false, false);
+
+		PrintMessage("Please Select your option (0-1): ");
+		cout<< ">";	cin >> n;
+		switch (n)
+		{
+		case 1:
+            AdminMenu();
+		    break;
+
+		    break;
+        case 0: exit(0);
+		default: "\a"; break;
+		}
+
+
+	} while (n != 0);
+
+	return 0;
 
 /*
     Loging l;
@@ -155,8 +235,115 @@ int main1()
         }
     }
     */
+
+    manageProjectMenu();
     return 0;
 }
+
+void AdminMenu()
+{
+
+    int n;
+    system("cls");
+	PrintMessage("ADMIN MANAGE EMPLOYEE");
+    PrintMessage("                               ", false, false);
+	PrintMessage("1.  Management Project         ", false, false);
+    PrintMessage("2.  Management Employee        ", false, false);
+    PrintMessage("                               ", false, false);
+	PrintMessage("0.  LOGOUT                     ", false, false);
+    PrintMessage("                               ", false, false);
+	PrintMessage("Enter Your Choice (0-5)");
+	cout<< ">";	cin >> n;
+	switch (n)
+	{
+	case 1:
+        ManageProjectMenu();
+	    break;
+	case 2:
+        ManageEmployeeMenu();
+        break;
+
+   	case 0:return;
+	default: cout << "\a";
+
+	}
+	AdminMenu();
+}
+
+
+void ManageEmployeeMenu()
+{
+    Employee e;
+    int n;
+    system("cls");
+	PrintMessage("ADMIN MANAGE EMPLOYEE");
+    PrintMessage("                               ", false, false);
+	PrintMessage("1.  Create Employee            ", false, false);
+    PrintMessage("2.  Update Employee            ", false, false);
+    PrintMessage("3.  Delete Employee            ", false, false);
+    PrintMessage("4.  Show all Employees         ", false, false);
+    PrintMessage("5.  Sign Employee as Admin     ", false, false);
+    PrintMessage("                               ", false, false);
+	PrintMessage("0.  LOGOUT                     ", false, false);
+    PrintMessage("                               ", false, false);
+	PrintMessage("Enter Your Choice (0-5)");
+	cout<< ">";	cin >> n;
+	switch (n)
+	{
+	case 1:
+	    //
+	    break;
+	case 2:
+        //
+        break;
+    case 3:
+        //
+	    break;
+    case 4:
+        e.show();
+	    break;
+   	case 0:return;
+	default: cout << "\a";
+
+	}
+	ManageEmployeeMenu();
+}
+
+void ManageProjectMenu()
+{
+    Project p;
+    int n;
+    system("cls");
+	PrintMessage("MANAGE PROJECT");
+    PrintMessage("                               ", false, false);
+	PrintMessage("1.  Show Project               ", false, false);
+    PrintMessage("2.  Add Project                ", false, false);
+    PrintMessage("3.  Deleted Project            ", false, false);
+    PrintMessage("4.  Update Project             ", false, false);
+    PrintMessage("                               ", false, false);
+	PrintMessage("0.  LOGOUT                     ", false, false);
+    PrintMessage("                               ", false, false);
+	PrintMessage("Enter Your Choice (0-4)");
+	cout<< ">";	cin >> n;
+	switch (n)
+	{
+	case 1:
+	    p.show();
+	    system("pause");
+	    break;
+	case 2:
+        //
+        break;
+    case 3:
+        //
+	    break;
+   	case 0:return;
+	default: cout << "\a";
+
+	}
+	ManageProjectMenu();
+}
+
 
 void manageEmployeeMenu(){
     int choice;
