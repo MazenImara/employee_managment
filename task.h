1c4ef17abb2c9864b7f7094579606535c3df9a48
ikram <ikrmo959@gmail.com>
2018-04-16 10:21:22 +0200

    changes in the attributes task.h

--- task.h
+++ task.h
@@ -5,13 +5,14 @@
 #include <windows.h>
 #include <gtime.h>
 
+
 using namespace std;
 
 
 class Task{
 public:
-    string id, title, status, time_spend, endtemp, starttemp, project_id, employee_id;
-    Employee e;
+    string id, title, status, timeSpend, endTemp, startTemp, projectId, employeeId;
+    //Employee e;
 
 public:
     void enterId(){
@@ -25,17 +26,11 @@ public:
        cout <<"|                                                      Task's details                                                              |"<<endl;
        cout <<"|__________________________________________________________________________________________________________________________________|"<<endl;
        cout <<"|"<<setw(5)<<"id"<<setw(20)<<"Title"<<setw(30)<<"Status"<<setw(20)<<"Time Spend"<<setw(30)<<"Start temp"<<setw(20)<<"End Temp"<<setw(5)<<"Employe Id"<<setw(5)<<"id"<<"|"<< endl;
-       cout <<"|"<<setw(5)<<id<<setw(20)<<title<<setw(30)<<status<<setw(20)<<time_spend<<setw(30)<<starttemp<<setw(20)<<endtemp<<setw(20)<<project_id<<setw(20)<<employee_id<<"|"<<endl;
+       cout <<"|"<<setw(5)<<id<<setw(20)<<title<<setw(30)<<status<<setw(20)<<timeSpend<<setw(30)<<startTemp<<setw(20)<<endTemp<<setw(20)<<projectId<<setw(20)<<employeeId<<"|"<<endl;
        cout <<"|__________________________________________________________________________________________________________________________________|"<<endl;
     }
-    /*
-    void show(){
-        cout<<"\nId: "<< id <<"\tTitle: "<< title <<"\tStatus: "<< status <<"\tTime Spend: "<< time_spend <<"\tDate Created: "<< date_created <<"\tProject id: "<< project_id <<"\tEmployee id: "<< employee_id <<endl;
-    }
-    */
 
-    void showAdd()
-    {
+    void showAdd(){
         cout<<"\nId: "<< id <<"\tTitle: "<< title << endl;
     }
 };
